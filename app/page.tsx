2f8b08e4c9b4a93b5840903b7a1b337149416207
BuildTools <unconfigured@null.spigotmc.org>
2023-12-02 18:34:47 +0900

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,111 @@
-import Image from 'next/image'
+"use client";
+
+import { useRef, useState } from "react";
 
 export default function Home() {
+  const [logMsg, setLogMsg] = useState<string>("");
+
+  let log = (_msg: string) => {
+    setLogMsg(_msg);
+  };
+
+  const rtcPeerRef = useRef<RTCPeerConnection | undefined>(undefined);
+  const mediaStreamRef = useRef<MediaStream | undefined>(undefined);
+  const [sdpInput, setSdpInput] = useState<string>();
+  const [sdpOutput, setSdpOutput] = useState<string>("");
+  const [iceInput, setIceInput] = useState<string>();
+  const [iceOutput, setIceOutput] = useState<string>("");
+  const iceCandidates = useRef<RTCIceCandidate[]>([]);
+
+  const selfVideoRef = useRef<HTMLVideoElement>(null);
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  let connect = async () => {
+    rtcPeerRef.current = new RTCPeerConnection({
+      iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
+    });
+
+    rtcPeerRef.current.oniceconnectionstatechange = (e) =>
+      log(rtcPeerRef.current?.iceConnectionState ?? "undefined");
+
+    rtcPeerRef.current!.onicecandidate = (event) => {
+      if (event.candidate === null) {
+        return;
+      }
+
+      iceCandidates.current.push(event.candidate);
+      setIceOutput(JSON.stringify(iceCandidates.current, null, 2));
+    };
+
+    // Create and get media stream
+    mediaStreamRef.current = await navigator.mediaDevices.getUserMedia({
+      video: { width: 800, height: 600 },
+    });
+
+    if (selfVideoRef.current !== null) {
+      selfVideoRef.current.srcObject = mediaStreamRef.current;
+    }
+
+    mediaStreamRef.current
+      .getTracks()
+      .forEach((track) =>
+        rtcPeerRef.current?.addTrack(track, mediaStreamRef.current!)
+      );
+
+    rtcPeerRef.current.ontrack = (event) => {
+      console.log("ontrack", event);
+      console.log("stream", event.streams.length);
+      if (videoRef.current !== null) {
+        videoRef.current.srcObject = event.streams[0];
+        videoRef.current.autoplay = true;
+        videoRef.current.controls = true;
+      }
+    };
+  };
+
+  let SDPOutput = async () => {
+    let sessionDescription = await rtcPeerRef.current?.createOffer();
+    await rtcPeerRef.current?.setLocalDescription(sessionDescription);
+    setSdpOutput(JSON.stringify(sessionDescription, null, 2));
+  };
+
+  let SDPInput = async () => {
+    const sessionDescription: RTCSessionDescriptionInit = JSON.parse(
+      sdpInput ?? ""
+    );
+    await rtcPeerRef.current?.setRemoteDescription(sessionDescription);
+
+    // Offer SDPの場合はAnswer SDPを作成
+    if (sessionDescription.type === "offer") {
+      const sessionDescription = await rtcPeerRef.current?.createAnswer();
+      await rtcPeerRef.current?.setLocalDescription(sessionDescription);
+      setSdpOutput(JSON.stringify(sessionDescription, null, 2));
+    }
+  };
+
+  let IceInput = async () => {
+    const iceCandidates: RTCIceCandidateInit[] = JSON.parse(iceInput ?? "");
+    for (const iceCandidate of iceCandidates) {
+      await rtcPeerRef.current?.addIceCandidate(iceCandidate);
+    }
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="flex flex-col">
+        <video width={800} height={600} autoPlay muted ref={selfVideoRef} />
+        <video width={800} height={600} autoPlay muted ref={videoRef} />
+        <button onClick={connect}>Connect</button>
+        <button onClick={SDPOutput}>SDP Output</button>
+        <textarea readOnly value={sdpOutput}></textarea>
+        <button onClick={SDPInput}>SDP Input</button>
+        <textarea onChange={(e) => setSdpInput(e.target.value)}></textarea>
+        <h1>ICE Output</h1>
+        <textarea readOnly value={iceOutput}></textarea>
+        <button onClick={IceInput}>Ice Input</button>
+        <textarea onChange={(e) => setIceInput(e.target.value)}></textarea>
+        <div>{logMsg}</div>
       </div>
     </main>
-  )
+  );
 }
